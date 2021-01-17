d0a6011bbace7ebf8f80f4a9e1fa6314e1e549d7
Molkobain <lajarige.guillaume@free.fr>
2021-01-17 18:36:57 +0100

    Add french translations

---
+++ fr.dict.ahws-dashlet-stats.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2013 XXXXX
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('FR FR', 'French', 'Français', array(
+	// Dictionary entries go here
+	'UI:DashletStats:Label' => 'Dashlet de stats.',
+	'UI:DashletStats:Description' => 'Dashlet badge groupé selon une fonction d\'aggrégation',
+	'UI:DashletStats:Prop:Title' => 'Titre',
+	'UI:DashletStats:Prop:Query' => 'Requête',
+	'UI:DashletStats:Prop:Function' => 'Fonction d\'aggrégation',
+	'UI:DashletStats:Prop:Function:Count' => 'Nombre',
+	'UI:DashletStats:Prop:Function:Max' => 'Maximum',
+	'UI:DashletStats:Prop:Function:Avg' => 'Moyenne',
+	'UI:DashletStats:Prop:Function:Min' => 'Minimum',
+	'UI:DashletStats:Prop:Function:Sum' => 'Somme',
+	'UI:DashletStats:Prop:Function:Percentage' => 'Pourcentage',
+
+	'UI:DashletStats:Prop:FunctionAttribute' => 'Attribut d\'aggrégation',
+	'UI:DashletStats:Prop:Unit' => 'Unité',
+	'UI:DashletStats:Prop:UnitPosition' => 'Position de l\'unité',
+	'UI:DashletStats:Prop:UnitPosition:Before' => 'Avant',
+	'UI:DashletStats:Prop:UnitPosition:After' => 'Après',
+	'UI:DashletStats:Prop:Function:PercentageQuery' => 'Requête en pourcentage',
+
+
+	'UI:DashletStatsCompare:Label' => 'Dashlet de stats. comparatives',
+	'UI:DashletStatsCompare:Description' => 'Dashlet badge pour comparer deux fonctions d\'aggrégation',
+	'UI:DashletStatsCompare:Prop:CompareQuery' => 'Requête de comparaison',
+	'UI:DashletStatsCompare:Prop:CompareUnit' => 'Unité de comparaison del\'écart',
+	'UI:DashletStatsCompare:Prop:CompareUnit:Difference' => 'Différence',
+	'UI:DashletStatsCompare:Prop:CompareUnit:Difference' => 'Pourcentage',
+	'UI:DashletStatsCompare:Prop:PercentageCompareQuery' => 'Requête de comparaison en pourcentage',
+
+));
